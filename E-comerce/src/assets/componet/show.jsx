f0859bafbc62075af207b72d1e03ad73ee833cd9
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2025-07-11 23:13:59 +0530

    update

--- E-comerce/src/assets/componet/show.jsx
+++ E-comerce/src/assets/componet/show.jsx
@@ -2,31 +2,48 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const Show=()=>{
-    const {id} = useParams();
-    const [showdata,setShowdata] = useState({});
+const Show = () => {
+    const { id } = useParams();
+    const [showdata, setShowdata] = useState({});
 
-    const onlaod=async()=>{
+    const onlaod = async () => {
         let api = `http://localhost:3000/clothes/${id}`
         let res = await axios.get(api);
         console.log(res.data);
         setShowdata(res.data);
     }
-    useEffect(()=>{
+    useEffect(() => {
         onlaod();
-    },[])
+    }, [])
 
-    return(
+    return (
         <>
-        <h1>helo</h1>
-        <div>
-            <div>
-                <img src={showdata.image} alt="image" />
+            <h1>helo</h1>
+            <div style={{display:"flex",justifyContent:"center"}}>
+                <div style={{backgroundColor:"black",padding:"10px",display:"flex"}}>
+                <div>
+                    <img src={showdata.image} alt="image" style={{height:"300px"}}/>
+                </div>
+                <div>
+                <div>
+                    <h4>{showdata.name}</h4>
+                </div>
+                <div>
+                    <p>{showdata.description}</p>
+                </div>
+                <div>
+                    <p>{showdata.rating}</p>
+                </div>
+                <div>
+                    <h3>{showdata.price} &nbsp;&nbsp; MRP:<span style={{color:"gray",textDecoration:"line-through"}}> {showdata.OldPrice}</span></h3>
+                </div>
+                <div>
+                    <button>Add</button>
+                    <button>Heart</button>
+                </div>
+                </div>
+                </div>
             </div>
-            <div>
-                <h3>{showdata.price}</h3>
-            </div>
-        </div>
         </>
     )
 }
--- todo/src/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import myfile from "./todoSlice"
-
-const Store = configureStore({
-    reducer:{
-      todo:myfile
-    }
-})
-export default Store;
